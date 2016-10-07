f8a5b5a6c376fd98b35c6ae7b4a36c562e081f2a
Anthony Fu <anthonyfu117@hotmail.com>
2016-10-08 00:47:24 +0800

    Refactoring 1

---
+++ chrome-ext/utils/app.js
@@ -0,0 +1,1 @@
+(new Vue({ mixins: [mixin] })).listen()
--- chrome-ext/utils/mixin.js
+++ chrome-ext/utils/mixin.js
@@ -1,6 +1,38 @@
 var mixin = {
+  el: '#vue-root',
+  data: {
+    watchers: {},
+    configs: {
+      background_refresh: false,
+      background_timeout: 1800000,
+      check_on_start: false,
+    },
+    listening: false
+  },
+  watch: {
+    configs: {
+      deep: true,
+      immediate: true,
+      handler: function functionName(val) {
+        if (this.listening)
+          storage.configs.update(val)
+      }
+    }
+  },
   methods: {
-    less: function(data, length) {
+    listen: function () {
+      if (this.listening)
+        return
+      var vm = this
+      storage.watchers.listen(function (watchers) {
+        vm.watchers = watchers
+      })
+      storage.configs.listen(function (new_configs) {
+        vm.configs = new_configs
+      })
+      this.listening = true
+    },
+    less: function (data, length) {
       length = length || 100
       var str = (data || '').toString().trim()
       if (str.length <= length)
@@ -8,41 +40,41 @@ var mixin = {
       else
         return str.slice(0, length) + ' ...'
     },
-    navigate: function(watcher) {
-      chrome.tabs.create({'url': watcher.url}, function(tab) {
+    navigate: function (watcher) {
+      chrome.tabs.create({ 'url': watcher.url }, function (tab) {
         chrome.tabs.executeScript(tab.id, {
-            code: 'WATCHER_CHECK("'+watcher.xpath+'");',
-            runAt: 'document_idle'
+          code: 'WATCHER_CHECK("' + watcher.xpath + '");',
+          runAt: 'document_idle'
         })
       })
     },
-    options: function() {
-      chrome.tabs.create({'url': chrome.extension.getURL('options/options.html')})
+    options: function () {
+      chrome.tabs.create({ 'url': chrome.extension.getURL('pages/options.html') })
     },
-    refresh: function(watcher, method) {
-      chrome.runtime.sendMessage({to: 'background', type: 'refresh', method:method, id:watcher.id})
+    refresh: function (watcher, method) {
+      chrome.runtime.sendMessage({ to: 'background', type: 'refresh', method: method, id: watcher.id })
     },
-    reset: function(watcher) {
+    reset: function (watcher) {
       storage.watchers.reset(watcher)
     },
-    refresh_all: function(method) {
-      chrome.runtime.sendMessage({to: 'background', type: 'refresh_all', method:method})
+    refresh_all: function (method) {
+      chrome.runtime.sendMessage({ to: 'background', type: 'refresh_all', method: method })
     },
-    remove: function(watcher) {
+    remove: function (watcher) {
       if (confirm("Are your sure to remove this watcher?"))
         storage.watchers.remove(watcher.id)
     },
-    remove_all: function() {
+    remove_all: function () {
       if (confirm("Are your sure to remove all watchers?"))
-      storage.watchers.clear()
+        storage.watchers.clear()
     },
-    humandate: function(datastr) {
+    humandate: function (datastr) {
       return moment(datastr || '').fromNow()
     },
-    export_data: function() {
+    export_data: function () {
       function download(text, name, type) {
         var a = document.createElement("a");
-        var file = new Blob([text], {type: type});
+        var file = new Blob([text], { type: type });
         a.href = URL.createObjectURL(file);
         a.download = name;
         document.body.appendChild(a);
@@ -51,7 +83,22 @@ var mixin = {
         delete a;
       }
 
-      download(storage.watchers.json(), 'tracker_export_'+moment().format('YYMMDD_hhmmss')+'.json', 'application/json')
+      download(storage.watchers.json(), 'tracker_export_' + moment().format('YYMMDD_hhmmss') + '.json', 'application/json')
+    },
+    open_panel: function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { to: 'dialog', type: 'toggle' })
+      })
+    },
+    upload_and_import: function(e) {
+      var files = e.target.files
+      var file = files[0]
+      var reader = new FileReader()
+      reader.onload = function () {
+        console.log(this.result)
+        storage.watchers.import(this.result)
+      }
+      reader.readAsText(file)
     }
   }
 }
